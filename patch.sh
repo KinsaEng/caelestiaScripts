4b6ae861ac32bbc26743be99f7d921269e087084
Kinsa <146025744+KinsaEng@users.noreply.github.com>
2026-03-18 17:34:33 +0300

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -40,13 +40,13 @@ echo -e "${CYAN}==>${NC} Available patches:"
 echo -e " ${CYAN}1) 24-hour clock"
 echo -e " 2) Celsius"
 echo -e " 3) Notifications container$ (experimental)"
-echo -e " 4) Power options"
+echo -e " 4) Power options" (exclusion recommended)
 echo -e " 5) Discord -> Signal"
-echo -e " 6) GIF patch"
+echo -e " 6) GIF patch" (exclusion recommended)
 echo -e " 7) Lockscreen text"
 echo
 
-echo -e " ==> ${NC} " 'Patches to exclude: (eg: "1 2 3", "1-3" or ENTER to skip)  ' "${CYAN}"
+echo -e " ==> ${NC} " 'Patches to exclude: (eg: "1 2 3", "1-3" or ENTER to skip, recommended "4 6")  ' "${CYAN}"
 read -p ' ==>' EXCLUDES
 echo -e "${NC}"
 # Parse ranges (1-3 etc.)
