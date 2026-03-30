f66988ac385b9d4becd9d9bd8a5a4d224b1d297e
Kinsa <146025744+KinsaEng@users.noreply.github.com>
2026-03-30 17:28:40 +0300

    Update switchos.sh

--- switchos.sh
+++ switchos.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+###################################################
+## DONT FORGET TO CHANGE WITH YOUR SUDO PASSWORD ##
+## ALSO MOVE THIS SCRIPT TO /usr/bin/switchos.sh ##
+## cp ./switchos.sh /usr/bin/switchos.sh         ##
+###################################################
+
 
 WINDOWS_ID=$(efibootmgr | awk '/Windows Boot Manager/ {print substr($1,5,4)}')
 
 echo "ID: $WINDOWS_ID"
-###################################################
-## DONT FORGET TO CHANGE WITH YOUR SUDO PASSWORD ##
-###################################################
+
 echo $YOUR_SUDO_PASS | sudo -S efibootmgr -n $WINDOWS_ID
+reboot
